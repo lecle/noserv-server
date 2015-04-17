a8f105d9921563edce2910aeb4ee50c0351cc3bb
daesun <yume@yumes.net>
2015-04-17 14:00:41 +0900

    add the cache clearing

--- test/config-test.js
+++ test/config-test.js
@@ -2,12 +2,15 @@ describe('config', function() {
     describe('#getConfig()', function () {
         it('should getConfig without error', function (done) {
 
+            delete require.cache[require.resolve('../conf/config')];
             process.env.NODE_ENV = 'local';
             var config = require('../conf/config');
 
+            delete require.cache[require.resolve('../conf/config')];
             process.env.NODE_ENV = 'deploy';
             config = require('../conf/config');
 
+            delete require.cache[require.resolve('../conf/config')];
             process.env.NODE_ENV = 'test';
             config = require('../conf/config');
 
