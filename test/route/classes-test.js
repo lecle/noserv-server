e650e439a194e4ea7d6531b30593c20541bf1199
daesun <yume@yumes.net>
2015-06-26 15:02:23 +0900

    add the updating auth test

--- test/route/classes-test.js
+++ test/route/classes-test.js
@@ -47,13 +47,53 @@ describe('classes-route', function() {
     describe('update', function() {
         it('should update without error', function(done) {
 
-            client.post('/1/classes/test', {test:'data'}, function (err, req, res, obj) {
+            client.post('/1/classes/test', {test:'data', _test:'data'}, function (err, req, res, obj) {
 
-                client.put('/1/classes/test/' + obj.objectId, {test:'updatedata'}, function (err, req, res, obj) {
+                var objId = obj.objectId;
+
+                client.put('/1/classes/test/' + objId, {test:'updatedata', _test:'updatedata'}, function (err, req, res, obj) {
 
                     assert.equal(200, res.statusCode);
                     assert(obj.updatedAt);
-                    done(err);
+
+                    client.get('/1/classes/test/' + objId, function (err, req, res, obj) {
+
+                        assert.equal(200, res.statusCode);
+                        assert.equal('updatedata', obj.test);
+                        assert.equal('updatedata', obj._test);
+                        done(err);
+                    });
+                });
+            });
+        });
+
+        it('should update without _test column', function(done) {
+
+            var client = restify.createJsonClient({
+                url: 'http://localhost:3337',
+                version: '~1.0',
+                headers: {
+                    'X-Noserv-Session-Token' : 'usertoken',
+                    'X-Noserv-Application-Id' : 'usertoken'
+                }
+            });
+
+            client.post('/1/classes/test', {test:'data', _test:'data'}, function (err, req, res, obj) {
+
+                var objId = obj.objectId;
+
+                client.put('/1/classes/test/' + objId, {test:'updatedata', _test:'updatedata'}, function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    assert(obj.updatedAt);
+
+                    client.get('/1/classes/test/' + objId, function (err, req, res, obj) {
+
+                        assert.equal(200, res.statusCode);
+                        assert.equal('updatedata', obj.test);
+                        assert.equal('data', obj._test);
+                        done(err);
+                    });
                 });
             });
         });
