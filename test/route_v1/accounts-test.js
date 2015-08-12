58efda21e4b574f73edb1825e9f9c1f28a52a5ca
daesun <yume@yumes.net>
2015-08-12 16:57:01 +0900

    add the v1 test

---
+++ test/route_v1/accounts-test.js
@@ -0,0 +1,139 @@
+var assert = require('assert');
+var container = require('../../lib/container').getInstance();
+var restify = require('restify');
+
+describe('accounts-route', function() {
+
+    var client = null;
+    var userId = null;
+
+    before(function(done) {
+        container.init('', '', function(err) {
+
+            setTimeout(done, 1000);
+        });
+
+        client = restify.createJsonClient({
+            url: 'http://localhost:3337',
+            version: '~1.0',
+            headers: {
+                'X-Noserv-Session-Token' : 'supertoken',
+                'X-Noserv-Application-Id' : 'supertoken'
+            }
+        });
+    });
+
+    after(function(done) {
+        container.close(function() {
+
+            setTimeout(done, 2000);
+        });
+    });
+
+    beforeEach(function(done) {
+
+        client.del('/v1/accounts', function(err, req, res, obj) {
+
+            client.post('/v1/accounts', {username:'test', password:'pass', phone:'010-1111-2222'}, function (err, req, res, obj) {
+
+                if(obj) {
+
+                    userId = obj.id;
+                }
+                done();
+            });
+        });
+    });
+
+    describe('create', function() {
+        it('should create without error', function(done) {
+
+            client.post('/v1/accounts', {username:'test2', password:'pass', phone:'010-1111-2222'}, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                assert(obj.createdAt);
+                assert(obj.id);
+                assert(obj.sessionToken);
+
+                done(err);
+            });
+        });
+    });
+
+    describe('login', function() {
+        it('should login without error', function(done) {
+
+            client.get('/v1/login?username=test&password=pass', function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('read', function() {
+        it('should read without error', function(done) {
+
+            client.get('/v1/accounts/' + userId, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('readMe', function() {
+        it('should readMe without error', function(done) {
+
+            client.get('/v1/accounts/me', function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('update', function() {
+        it('should update without error', function(done) {
+
+            client.put('/v1/accounts/' + userId, {username:'test', password:'pass', phone:'010-1111-2222'}, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('find', function() {
+        it('should find without error', function(done) {
+
+            client.get('/v1/accounts?username=test', function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('destroy', function() {
+        it('should destroy without error', function(done) {
+
+            client.del('/v1/accounts/' + userId, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+
+    describe('resetPassword', function() {
+        it('should resetPassword without error', function(done) {
+
+            client.post('/v1/requestPasswordReset', {email:'test@test.com'}, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode);
+                done();
+            });
+        });
+    });
+});
---
+++ test/route_v1/entities-test.js
@@ -0,0 +1,147 @@
+var assert = require('assert');
+var container = require('../../lib/container').getInstance();
+var restify = require('restify');
+
+describe('entities-route', function() {
+
+    var client = null;
+    var userId = null;
+
+    before(function(done) {
+        container.init('', '', function(err) {
+
+            setTimeout(done, 1000);
+        });
+
+        client = restify.createJsonClient({
+            url: 'http://localhost:3337',
+            version: '~1.0',
+            headers: {
+                'X-Noserv-Session-Token' : 'supertoken',
+                'X-Noserv-Application-Id' : 'supertoken'
+            }
+        });
+    });
+
+    after(function(done) {
+        container.close(function() {
+
+            setTimeout(done, 2000);
+        });
+    });
+
+    describe('create', function() {
+        it('should create without error', function(done) {
+
+            client.post('/v1/entities/test', {test:'data'}, function (err, req, res, obj) {
+
+                assert.equal(200, res.statusCode, JSON.stringify(obj));
+                assert(obj.createdAt);
+                assert(obj.id);
+
+                done(err);
+            });
+        });
+    });
+
+    describe('update', function() {
+        it('should update without error', function(done) {
+
+            client.post('/v1/entities/test', {test:'data', _test:'data'}, function (err, req, res, obj) {
+
+                var objId = obj.id;
+
+                client.put('/v1/entities/test/' + objId, {test:'updatedata', _test:'updatedata'}, function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    assert(obj.updatedAt);
+
+                    client.get('/v1/entities/test/' + objId, function (err, req, res, obj) {
+
+                        assert.equal(200, res.statusCode);
+                        assert.equal('updatedata', obj.test);
+                        assert.equal('updatedata', obj._test);
+                        done(err);
+                    });
+                });
+            });
+        });
+
+        it('should update without _test column', function(done) {
+
+            var client = restify.createJsonClient({
+                url: 'http://localhost:3337',
+                version: '~1.0',
+                headers: {
+                    'X-Noserv-Session-Token' : 'usertoken',
+                    'X-Noserv-Application-Id' : 'usertoken'
+                }
+            });
+
+            client.post('/v1/entities/test', {test:'data', _test:'data'}, function (err, req, res, obj) {
+
+                var objId = obj.id;
+
+                client.put('/v1/entities/test/' + objId, {test:'updatedata', _test:'updatedata'}, function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    assert(obj.updatedAt);
+
+                    client.get('/1/classes/test/' + objId, function (err, req, res, obj) {
+
+                        assert.equal(200, res.statusCode);
+                        assert.equal('updatedata', obj.test);
+                        assert.equal('data', obj._test);
+                        done(err);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('read', function() {
+        it('should read without error', function(done) {
+
+            client.post('/v1/entities/test', {test:'data'}, function (err, req, res, obj) {
+
+                client.get('/v1/entities/test/'+ obj.id, function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    assert.equal(obj.test, 'data');
+                    done(err);
+                });
+            });
+        });
+    });
+
+    describe('find', function() {
+        it('should find without error', function(done) {
+
+            client.post('/v1/entities/test', {test:'data'}, function (err, req, res, obj) {
+
+                client.get('/v1/entities/test?where={"test":"data"}', function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    assert(obj.results);
+                    assert(obj.results.length > 0);
+                    assert.equal(obj.results[0].test, 'data');
+                    done(err);
+                });
+            });
+        });
+    });
+
+    describe('destroy', function() {
+        it('should destroy without error', function(done) {
+
+            client.post('/v1/entities/test', {test:'deletedata'}, function (err, req, res, obj) {
+
+                client.del('/v1/entities/test/'+ obj.id, function (err, req, res, obj) {
+
+                    assert.equal(200, res.statusCode);
+                    done(err);
+                });
+            });
+        });
+    });
+});
